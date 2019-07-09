c577e8c129b1ca5a0c26dee4a20575a4fdb5c66c
Celal Cankaya <celalcankaya@outlook.com>
2019-07-09 15:37:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 && number % 5
+    return "FizzBuzz"
+  end
+  if number % 3
+    return "Fizz"
+  end
+  if number % 5
+    return "Buzz"
+end
